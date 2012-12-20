8a28c0fdbbbb5b273b698e1d9b9f17f8db2e064e
csanghvi <csanghvi@tavisca.com>
2012-12-20 17:46:30 +0530

    added meetuplist view

--- connect/Scripts/views/meetupListView/meetupListView.js
+++ connect/Scripts/views/meetupListView/meetupListView.js
@@ -2,9 +2,21 @@
 if (!Connect.views) Connect.views = {};
 
 window.Connect.views.meetupListView = Backbone.View.extend({
+
     templateName: 'meetupListTemplate',
 
-    render: function () {
+    render: function ($container) {
+        this.$container = $container;
+        var html = Mustache.render(this.template, { listContainer: {} });
+        this.$container.empty().append(html);
+        
+        this.$el = $('#meetup-list-container', this.$container);
+        this.appendMeetups();
+    },
+
+    appendMeetups: function () {
+        var html = Mustache.render(this.template, { list: {} });
+        this.$el.append(html);
     }
 });
   
