44c79840a53db4455cde5b19b93d13df8e1d4eab
Biswarup Chakravarty <bchakravarty@appacitive.com>
2012-12-21 22:09:19 +0530

    added meetups organized listing.

--- connect/Scripts/controllers/meetupController.js
+++ connect/Scripts/controllers/meetupController.js
@@ -85,7 +85,16 @@ Connect.controllers.meetupController = new (function () {
             if (!args.isAdd) {
                 //fetch attending meetups for current user and call callback _c
                 var meetups = [];
-                _c(meetups);
+                var users = new Appacitive.ArticleCollection({ schema: 'user' });
+                var thisUser = users.createNewArticle();
+                thisUser.set('__id', Connect.bag.user.__id);
+                var meetupsOrganized = thisUser.getConnectedArticles({ relation: 'user_meetup', otherSchema: 'meetup' });
+                meetupsOrganized.fetch(function () {
+                    if (meetupsOrganized.getAll().length > 0) {
+                        meetups = meetupsOrganized.getAll().map(function (m) { return m.connectedArticle.getArticle(); });
+                    }
+                    _c(meetups);
+                });
             } else {
                 base.meetupAttendView.model.meetups.splice(0, 0, args.meetup);
                 base.meetupAttendView.render();
