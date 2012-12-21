ccbdaa58c7327bb28a36269159a22db0b110fb70
Amar Palsapure <apalsapure@tavisca.com>
2012-12-21 16:19:11 +0530

    sdk session create added

--- connect/Scripts/controllers/applicationController.js
+++ connect/Scripts/controllers/applicationController.js
@@ -4,54 +4,13 @@ if (!Connect.controllers) Connect.controllers = {};
 
 
 Connect.controllers.applicationController = new (function () {
-    var sessionId = null;
-
-    this.getSessionId = function () { return sessionId || null; };
-
-    this.clearSessionId = function () { $.removeCookie('__connect_app_session'); };
-
     this.createSession = function () {
-        var request = {
-            apikey: Connect.config.apikey,
-            isnonsliding: false,
-            usagecount: -1,
-            windowtime: 240
-        };
-        
-        var url = Connect.storage.urlFactory.session.getCreateSessionUrl();
-        Connect.utils.ajax.put(url, request, false, function (data) {
-            if (data.session && data.session.sessionkey) {
-                sessionId = data.session.sessionkey;
-                Connect.utils.cookies.set('__connect_app_session', sessionId);
-                EventManager.fire('sessionCreated');
-            } else {
-                throw new Error("Couldn't get session key from server.");
-            }
-        }, function () {
-            throw new Error("Couldn't get session key from server.");
-        });
-    };
-
-    this.refreshSession = function () {
-        var that = this;
-        var url = Connect.storage.urlFactory.article.getSearchAllUrl('entity');
-        Connect.utils.ajax.get(url, true, function (data) {
-            if (data) {
-                if (data.status && data.status != null) {
-                    if (data.status.code == '8027' || data.status.code == '8002' || data.status.code == '8036') {
-                        that.createSession();
-                    }
-                }
-            }
-        }, function () {
-            throw new Error("Couldn't connect to server.");
+        var _sessionOptions = { "apikey": Connect.config.apikey, app: 'sdk' }
+        Appacitive.session.create(_sessionOptions);
+        Appacitive.eventManager.subscribe('session.success', function () {
+            EventManager.fire('sessionCreated');
         });
     };
 })();
 
 EventManager.subscribe('root.pageInit', Connect.controllers.applicationController.createSession);
-
-// to refresh api session
-window.setInterval(function () {
-    Connect.controllers.applicationController.refreshSession();
-}, 5 * 60 * 1000);
---
+++ connect/Scripts/infrastructure/sdk/EventManager.js
@@ -0,0 +1,126 @@
+/**
+Depends on  NOTHING
+**/
+
+(function(global) {
+    
+    /**
+     * @constructor
+    */
+
+    var EventManager = function () {
+
+        function GUID() {
+            var S4 = function () {
+                return Math.floor(
+                    Math.random() * 0x10000 /* 65536 */
+                ).toString(16);
+            };
+
+            return (
+                S4() + S4() + "-" +
+                S4() + "-" +
+                S4() + "-" +
+                S4() + "-" +
+                S4() + S4() + S4()
+            );
+        }
+
+        var _subscriptions = {};
+
+        this.subscribe = function (eventName, callback) {
+            if (typeof (eventName) != "string" || typeof (callback) != "function")
+                throw new Error("Incorrect subscription call");
+
+            if (typeof (_subscriptions[eventName]) == "undefined")
+                _subscriptions[eventName] = [];
+
+            var _id = GUID()
+            _subscriptions[eventName].push({
+                callback: callback,
+                id: _id
+            });
+
+            return _id;
+        };
+
+        this.unsubscribe = function (id) {
+            if (!id) return false;
+            var index = -1, eN = null
+            for (var eventName in _subscriptions) {
+                for (var y = 0; y < _subscriptions[eventName].length; y = y + 1) {
+                    if (_subscriptions[eventName][y].id == id) {
+                        index = y;
+                        eN = eventName;
+                        break;
+                    }
+                }
+            }
+            if (index != -1) {
+                _subscriptions[eN].splice(index, 1);
+                return true;
+            }
+            return false;
+        };
+
+        this.fire = function (eventName, sender, args) {
+            if (typeof (eventName) != "string") throw new Error("Incorrect fire call");
+
+            if (typeof (args) == "undefined" || args == null)
+                args = {};
+            args.eventName = eventName;
+
+            // shifted logging here
+            // for better debugging
+            if (console && console.log && typeof console.log == 'function')
+                console.log(eventName + ' fired');
+
+            if (typeof (_subscriptions["all"]) != "undefined") {
+                for (var x = 0; x < _subscriptions["all"].length; x = x + 1) {
+                    //try {
+                    _subscriptions["all"][x].callback(sender, args);
+                    //} catch (e) { }
+                }
+            }
+
+            if (typeof (_subscriptions[eventName]) != "undefined") {
+                for (var x = 0; x < _subscriptions[eventName].length; x = x + 1) {
+                    (function (f, s, a) {
+                        setTimeout(function () {
+                            f(s, a);
+                        }, 0);
+                    })(_subscriptions[eventName][x].callback, sender, args);
+                }
+            }
+        };
+
+        this.clearSubscriptions = function (eventName) {
+            if (typeof (eventName) != 'string')
+                throw new Error('Event Name must be string in EventManager.clearSubscriptions');
+
+            if (_subscriptions[eventName]) _subscriptions[eventName].length = 0;
+
+            return this;
+        };
+
+        this.clearAndSubscribe = function (eventName, callback) {
+            this.clearSubscriptions(eventName);
+            this.subscribe(eventName, callback);
+        };
+
+        this.dump = function () {
+            console.dir(_subscriptions);
+        };
+
+    }
+
+    global.Appacitive.eventManager = new EventManager();
+
+})(window || process);
+
+/// unit test
+var _function = function(s, a) {
+    console.log('i subscribe to ' + a.eventName);
+}
+
+window.Appacitive.eventManager.fire('unit.test');
